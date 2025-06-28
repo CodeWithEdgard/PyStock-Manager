e1ba149312cfe5e4b77a1211b9d7958960a7f3d2
Edgard <mendesbr92@gmail.com>
2025-06-28 12:44:03 -0300

    feat(api): complete all CRUD endpoints for products

--- app/crud.py
+++ app/crud.py
@@ -34,3 +34,11 @@ def update_product(db: Session, product_id: int, product_data: schemas.ProductCr
   db.refresh(db_product)
   
   return db_product
+
+def delete_product(db: Session, product_id: int):
+  db_product = db.query(models.Product).filter(models.Product.id == product_id).first()
+  
+  if db_product:
+    db.delete(db_product)
+    db.commit()
+  return db_product
